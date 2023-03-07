aca7ff2664a99329a28e0a37da4bfac78256d351
Maya Bachvarova <may13@abv.bg>
2023-03-07 11:20:38 +0200

    Objects and Classes exercices

--- Objects_and_classes_exercise/6.wordsTracker.js
+++ Objects_and_classes_exercise/6.wordsTracker.js
@@ -2,75 +2,60 @@ function wordsTracker(arr) {
   let searched = arr.shift().split(" ");
 
   let wordsCount = {};
+  for (const search of searched) {
+    wordsCount[search] = 0;
+  }
+
   for (const el of arr) {
-    if (wordsCount.hasOwnProperty(el)) {
+    if (el in wordsCount) {
       wordsCount[el] += 1;
-    } else {
-      wordsCount[el] = 1;
     }
   }
 
-  // console.log(wordsCount);
+  //   console.log(wordsCount);
 
-  let wordsCountSort = Object.entries(wordsCount)
-    .sort(([, a], [, b]) => b - a)
-    .reduce((r, [k, v]) => ({ ...r, [k]: v }), {});
-  // obj to arr > sort arr by value > arr to obj with reduce
+  let wordsCountSort = Object.entries(wordsCount).sort(([, a], [, b]) => b - a);
+  //   console.log(wordsCountSort);
 
-  for (const word in wordsCountSort) {
-    const found = searched.find((element) => element === word);
-    if (found) {
-      console.log(word, "-", wordsCountSort[word]);
-    }
+  for (const word of wordsCountSort) {
+    console.log(word[0], "-", word[1]);
   }
-
-  // find if key(word) is in searched with .find
-  // if key is in searched console.log
 }
-// console.log(wordsCountSort);
-// for (const word in wordsCountSort) {
-//   for (const search of searched) {
-//     if (word === search) {
-//       console.log(word, "-", wordsCountSort[word]);
-//     }
-//   }
-// const found = searched.find((element) => element === word);
-// if (found) {
-//   console.log(word, "-", wordsCountSort[word]);
-// }
-
-// find if key(word) is in searched with .find
-// if key is in searched console.log
-
-// 40/100
 
 wordsTracker([
-  "this sentence 1",
+  "this sentence",
   "In",
   "this",
   "sentence",
-  "sentence",
   "you",
-
-  "1",
-  "1",
-  "1",
-  "2",
+  "have",
+  "to",
+  "count",
+  "the",
+  "occurrences",
+  "of",
+  "the",
+  "words",
+  "this",
+  "and",
+  "sentence",
+  "because",
+  "this",
+  "is",
+  "your",
+  "task",
 ]);
-// wordsTracker([
-//   "is the",
-//   "first",
-//   "sentence",
-//   "is",
-//   "is",
-
-//   "Here",
-//   "is",
-//   "another",
-//   "the",
-//   "And",
-//   "finally",
-//   "the",
-//   "the",
-//   "sentence",
-// ]);
+wordsTracker([
+  "is the",
+  "first",
+  "sentence",
+  "Here",
+  "is",
+  "another",
+  "the",
+  "And",
+  "finally",
+  "the",
+  "the",
+  "sentence",
+]);
