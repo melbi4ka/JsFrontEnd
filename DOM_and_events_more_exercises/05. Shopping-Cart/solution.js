ace26e636d71328083a2f6ae3fd5849b0645a3e3
Maya Bachvarova <may13@abv.bg>
2023-03-17 13:00:11 +0200

    DOM and events More exercises

--- DOM_and_events_more_exercises/05. Shopping-Cart/solution.js
+++ DOM_and_events_more_exercises/05. Shopping-Cart/solution.js
@@ -1,3 +1,38 @@
 function solve() {
-   //TODO...
+  const addButton = Array.from(document.querySelectorAll("button.add-product"));
+  const checkoutButton = document.querySelector(".checkout");
+  const textArea = document.querySelector("textarea");
+
+  addButton.forEach((button) => button.addEventListener("click", addOnClick));
+  checkoutButton.addEventListener("click", checkoutOnClick);
+
+  let productList = [];
+  let total = 0;
+
+  function addOnClick(event) {
+    parentDiv = event.target.parentElement.parentElement;
+    const productName =
+      parentDiv.getElementsByClassName("product-title")[0].textContent;
+    const productPrice = Number(
+      parentDiv.getElementsByClassName("product-line-price")[0].textContent
+    );
+
+    if (!productList.includes(productName)) {
+      productList.push(productName);
+    }
+    total += productPrice;
+
+    textArea.textContent += `Added ${productName} for ${productPrice.toFixed(
+      2
+    )} to the cart.\n`;
+  }
+
+  function checkoutOnClick() {
+    textArea.textContent += `You bought ${productList.join(
+      ", "
+    )} for ${total.toFixed(2)}.`;
+
+    addButton.forEach((button) => (button.disabled = true));
+    checkoutButton.disabled = true;
+  }
 }
