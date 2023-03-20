46f6f99c840b68f69f4034297f9a13736f6559ae
Maya Bachvarova <may13@abv.bg>
2023-03-20 12:09:27 +0200

    DOM and events More exercises

--- DOM_and_events_more_exercises/10. Number Convertor/solution.js
+++ DOM_and_events_more_exercises/10. Number Convertor/solution.js
@@ -1,4 +1,27 @@
 function solve() {
+  const selectMenu = document.getElementById("selectMenuTo");
+  const input = document.getElementById("input");
+  const inputResult = document.getElementById("result");
 
-    //TODO...
+  const optionOne = Array.from(selectMenu.children)[0];
+  const optionTwo = optionOne.cloneNode(true);
+
+  optionOne.value = "binary";
+  optionOne.textContent = "Binary";
+  optionTwo.value = "hexadecimal";
+  optionTwo.textContent = "Hexadecimal";
+  selectMenu.appendChild(optionTwo);
+
+  const button = document.querySelector("button");
+  button.addEventListener("click", onClick);
+
+  function onClick() {
+    const info = Number(input.value);
+    const select = selectMenu.value;
+    if (select === "binary") {
+      inputResult.value = info.toString(2);
+    } else if (select === "hexadecimal") {
+      inputResult.value = info.toString(16).toUpperCase();
+    }
+  }
 }
--- DOM_and_events_more_exercises/11. Time-Converter/solution.js
+++ DOM_and_events_more_exercises/11. Time-Converter/solution.js
@@ -1,4 +1,48 @@
 function attachEventsListeners() {
+  const buttons = Array.from(document.querySelectorAll('input[type="button"]'));
+  const inputs = Array.from(document.querySelectorAll('input[type="text"]'));
 
-    console.log('TODO:...');
+  buttons.forEach((el) => el.addEventListener("click", onClick));
+  let mapper = {
+    days: (a) => a / 86400,
+
+    hours: (a) => a / 3600,
+    minutes: (a) => a / 60,
+    seconds: (a) => a,
+  };
+
+  function onClick(event) {
+    for (const el of inputs) {
+      console.log(el);
+      if (el.value && el.id == "days") {
+        const inSec = Number(el.value) * 86400;
+        addValue(inputs, inSec, mapper);
+      } else if (el.value && el.id == "hours") {
+        const inSec = Number(el.value) * 3600;
+        addValue(inputs, inSec, mapper);
+      } else if (el.value && el.id == "minutes") {
+        const inSec = Number(el.value) * 60;
+        addValue(inputs, inSec, mapper);
+      } else if (el.value && el.id == "seconds") {
+        const inSec = Number(el.value);
+        addValue(inputs, inSec, mapper);
+      }
+    }
+    function addValue(inputs, sec, mapper) {
+      for (const el of inputs) {
+        if (el.id === "days") {
+          el.value = mapper["days"](sec);
+        }
+        if (el.id == "minutes") {
+          el.value = mapper["minutes"](sec);
+        }
+        if (el.id == "hours") {
+          el.value = mapper["hours"](sec);
+        }
+        if (el.id == "seconds") {
+          el.value = mapper["seconds"](sec);
+        }
+      }
+    }
+  }
 }
