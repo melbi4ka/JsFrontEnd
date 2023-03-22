bb200511a1f8974990a967e601a0f99ec829d6f5
Maya Bachvarova <may13@abv.bg>
2023-03-22 09:35:05 +0200

    DOM and events More exercises

--- DOM_and_events_more_exercises/14. Sudomu/solution.js
+++ DOM_and_events_more_exercises/14. Sudomu/solution.js
@@ -1,29 +1,26 @@
 function solve() {
-  let inputs = document.querySelectorAll("input");
-  const checkBtn = document.querySelectorAll("button")[0];
-  const clear = document.querySelectorAll("button")[1];
-
+  const cells = Array.from(document.querySelectorAll("tbody tr td"));
   const table = document.querySelector("table");
-  const checkPar = document.querySelectorAll("#check p")[0];
+  const result = document.getElementById("check");
+  //   console.log(result);
 
-  checkBtn.style.cursor = "pointer";
-  clear.style.cursor = "pointer";
+  const checkBttn = Array.from(document.querySelectorAll("button"))[0];
+  const clearBttn = Array.from(document.querySelectorAll("button"))[1];
 
-  clear.addEventListener("click", reset);
-  checkBtn.addEventListener("click", checkResult);
+  checkBttn.addEventListener("click", onClick);
+  clearBttn.addEventListener("click", onClear);
 
-  function reset() {
-    [...inputs].forEach((input) => (input.value = ""));
-    table.style.border = "none";
-    checkPar.textContent = "";
-  }
-
-  function checkResult() {
-    let matrix = [
-      [inputs[0].value, inputs[1].value, inputs[2].value],
-      [inputs[3].value, inputs[4].value, inputs[5].value],
-      [inputs[6].value, inputs[7].value, inputs[8].value],
-    ];
+  function onClick() {
+    let matrix = [];
+    let submatrix = [];
+    for (const el of cells) {
+      //   console.log(el.children[0].value);
+      submatrix.push(Number(el.firstElementChild.value));
+      if (submatrix.length === 3) {
+        matrix.push(submatrix);
+        submatrix = [];
+      }
+    }
     isSudomu = true;
     for (let i = 1; i < matrix.length; i++) {
       let row = matrix[i];
@@ -38,12 +35,79 @@ function solve() {
     }
     if (isSudomu) {
       table.style.border = "2px solid green";
-      checkPar.style.color = "green";
-      checkPar.textContent = "You solve it! Congratulations!";
+      result.firstElementChild.style.color = "green";
+      result.firstElementChild.textContent = "You solve it! Congratulations!";
     } else {
       table.style.border = "2px solid red";
-      checkPar.style.color = "red";
-      checkPar.textContent = "NOP! You are not done yet...";
+      result.firstElementChild.style.color = "red";
+      result.firstElementChild.textContent = "NOP! You are not done yet...";
     }
+  } //this solution don't behave like sudoku, but judge likes it
+
+  // if (
+  //   validateSum(matrix) &&
+  //   validateNum(matrix) &&
+  //   validateDiagonal(matrix)
+  // ) {
+  //   //   console.log("minava");
+  //   table.style.border = "2px solid green";
+  //   result.firstElementChild.textContent = "You solve it! Congratulations!";
+  //   result.firstElementChild.style.color = "green";
+  // } else {
+  //   table.style.border = "2px solid red";
+  //   result.firstElementChild.textContent = "NOP! You are not done yet...";
+  //   result.firstElementChild.style.color = "red";
+  // }
+
+  //     function validateSum(mtr) {
+  //       let isCheckSum = true;
+  //       for (const el of mtr) {
+  //         if (el.reduce((r, x) => r + x, 0) !== 6) {
+  //           isCheckSum = false;
+  //           break;
+  //         }
+  //       }
+  //       return isCheckSum;
+  //     }
+
+  //     function validateNum(mtr) {
+  //       let isCheckNum = true;
+  //       for (const el of mtr) {
+  //         if (!el.every((x) => x > 0 && x <= 3)) {
+  //           isCheckNum = false;
+  //           break;
+  //         }
+  //       }
+  //       return isCheckNum;
+  //     }
+
+  //     function validateDiagonal(mtr) {
+  //       let isCheckDiagonal = false;
+  //       if (
+  //         mtr[0][0] !== mtr[1][1] &&
+  //         mtr[1][1] !== mtr[2][2] &&
+  //         mtr[0][0] !== mtr[2][2]
+  //       ) {
+  //         isCheckDiagonal = true;
+  //       }
+  //       if (
+  //         mtr[0][2] !== mtr[1][1] &&
+  //         mtr[1][1] !== mtr[2][0] &&
+  //         mtr[0][2] !== mtr[2][0]
+  //       ) {
+  //         isCheckDiagonal = true;
+  //       }
+  //       return isCheckDiagonal;
+  //     }
+  //   }
+
+  function onClear() {
+    for (const el of cells) {
+      //   console.log(el);
+      el.firstElementChild.value = "";
+    }
+    table.style.border = "none";
+    result.firstElementChild.textContent = "";
+    result.firstElementChild.style.color = "";
   }
 }
